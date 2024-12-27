0ce12202ee8718b1ef6d64b07758d88adb029cb1
Juane Elizondo <juaneelizondo@MacBook-Pro-de-Juane.local>
2024-12-26 21:13:57 -0300

    First issue solved

--- src/components/ImagenPrincipal.jsx
+++ src/components/ImagenPrincipal.jsx
@@ -6,7 +6,7 @@ const ImagenPrincipal = () => {
     <div className='h-screen'>
         <div className='flex md:hidden bg-fondo-save bg-center bg-no-repeat bg-cover bg-fixed'>
             <div className='flex h-screen w-full justify-center items-end'>
-                <img src={arrow} alt="" className='size-16 text-white animate-pulse mb-5'/>
+                <img src={arrow} alt="" className='size-16 text-white animate-pulse mb-10'/>
             </div>
         </div>
         <div className='hidden md:flex bg-fondo-save-desk bg-center bg-no-repeat bg-cover'>
