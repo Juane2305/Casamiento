31985889285a7ab02dce0fda013e069b3b958666
Juane Elizondo <juaneelizondo@MacBook-Pro-de-Juane.local>
2024-12-26 21:39:44 -0300

    cambio de letra 2

--- src/components/Fecha.jsx
+++ src/components/Fecha.jsx
@@ -7,7 +7,7 @@ const Fecha = () => {
         <div className='bg-[#EBDED4] h-72 flex flex-col justify-center items-center py-10'>
             <h1 className='mt-5 text-[2rem] font-thin'>¡Nos casamos!</h1>
             <img src={calendar} alt="" className='size-16'/>
-            <h2 className='text-[2rem] pt-3 font-light' >Guarda la fecha...</h2>
+            <h2 className='text-[2rem] pt-3 font-light' >Guarda la fecha</h2>
             <h2 className='text-[1.5rem] font-light'>6 de Abril a las 13:00hs.</h2>
             <h3 className='text-[1.5rem] font-light'>Salón Terra Oliva</h3>
         </div>
